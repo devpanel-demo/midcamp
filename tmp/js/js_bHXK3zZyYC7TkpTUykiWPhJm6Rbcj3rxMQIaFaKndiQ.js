7b0181eb928cc4ac11c2f0d95a50abc205cd6710
levi-devpanel <109947881+levidevpanel@users.noreply.github.com>
2025-03-18 03:38:20 +0000

    init source code

--- tmp/js/js_bHXK3zZyYC7TkpTUykiWPhJm6Rbcj3rxMQIaFaKndiQ.js
+++
@@ -1,2 +0,0 @@
-/* @license GPL-2.0-or-later https://www.drupal.org/licensing/faq */
-jQuery,Drupal,Drupal.behaviors.map={attach:function(a,e){mapboxgl.accessToken="pk.eyJ1IjoibWlkY2FtcCIsImEiOiJjaXV2czRteHQwNGhkMm9zNWp4bWhndTk0In0.F5jwcKus8Rzx5r8Ga-CRQA",new mapboxgl.Map({container:"map",style:"mapbox://styles/midcamp/ciuvsjxje00312ipqrfyxm4di",center:[-87.66,41.924],zoom:15}).scrollZoom.disable();}};;
